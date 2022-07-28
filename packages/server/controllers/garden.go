00268ab3e465a5ddf3b321bbcf5bf2c50bbebe35
yauer <yanis.auer@epitech.eu>
2022-07-28 15:27:40 +0200

    test get all garden

--- packages/server/controllers/garden.go
+++ packages/server/controllers/garden.go
@@ -13,8 +13,8 @@ type garden struct{}
 var Garden garden
 
 func (garden) GetAll(c *fiber.Ctx) error {
-	result := map[string]interface{}{}
-	database.DB.Model(&models.Garden{}).First(&result)
+	result := database.DB.Find(&Garden)
+	println("all")
 	return c.JSON(fiber.Map{"result": result})
 }
 
@@ -41,5 +41,23 @@ func (garden) CreateGarden(c *fiber.Ctx) error {
 			"reason":  err.Error(),
 		})
 	}*/
-		return nil
+	/*garden := Garden{Id: 10, Name: "ahaha", Width: 15, Height: 15}
+
+result := database.DB.Create(&garden) // pass pointer of data to Create
+
+user.ID             // returns inserted data's primary key
+result.Error        // returns error
+result.RowsAffected // returns inserted records count
+
+type Garden struct {
+	gorm.Model
+	Id              uint `gorm:"uniqueIndex"`
+	Name            *string
+	Width           int
+	Height          int
+	Paths           Position `gorm:"embedded"`
+	VectorPositions VectorPosition `gorm:"embedded"`
+	PlantList       int
+}*/
+	return nil
 }
