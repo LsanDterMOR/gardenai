bfc84f15fdd010917aeaab9602d96d804aeac4c4
iammwun <shokipsh@gmail.com>
2022-07-30 00:10:30 +0200

    add: utilities package

--- packages/server/main.go
+++ packages/server/main.go
@@ -1,22 +1,16 @@
 package main
 
 import (
+	"gardenai/server/database"
 	"gardenai/server/routes"
+	"gardenai/server/utilities"
 	"log"
-	"os"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func getEnv(key string, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
-	}
-	return fallback
-}
-
 func main() {
-	// database.Configure()
+	database.Configure()
 
 	app := fiber.New()
 	api := app.Group("/api")
@@ -25,7 +19,7 @@ func main() {
 	routes.User.Init(v1)
 	routes.Plant.Init(v1)
 
-	port := getEnv("PORT", "4000")
+	port := utilities.GetEnv("PORT", "4000")
 
 	log.Fatal(app.Listen(":" + port))
 }
