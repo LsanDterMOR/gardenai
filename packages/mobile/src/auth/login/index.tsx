c3473beb994d2be804486b7049324056dbc05524
fquene <florian.quene@epitech.eu>
2022-07-31 21:13:29 +0200

    [ADD] backend in mobile login/register

--- packages/mobile/src/auth/login/index.tsx
+++ packages/mobile/src/auth/login/index.tsx
@@ -1,93 +1,142 @@
-import { StyleSheet, Text, View, Button, TextInput, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import React, { useState } from 'react';
-import LogoIcon from '../src/LogoIcon';
-import LoginBtn from '../src/LoginBtn';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import React, { useState } from "react";
+import LogoIcon from "../src/LogoIcon";
+import LoginBtn from "../src/LoginBtn";
+import axios from "axios";
 
 interface LoginProps {
   navigation: any;
 }
 
 const Login = (props: LoginProps) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [isEmail, setIsEmail] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const moveToRegister = () => props.navigation.navigate("Register");
+  const moveToGardenai = () => props.navigation.navigate("Gardenai");
 
-  const moveToRegister = () => props.navigation.navigate("Register")
-  const moveToGardenai = () => props.navigation.navigate("Gardenai")
+  const startLogin = async () => {
+    try {
+      if (isEmail) {
+        const login = await fetch(
+          "https://gardenai-backend.herokuapp.com/api/v1/user/signin",
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email: email,
+              password: password,
+            }),
+          }
+        );
+        if (login.status == 200) {
+          setIsError(false);
+          moveToGardenai();
+        } else {
+          setIsError(true);
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   function validateEmail(text: React.SetStateAction<string>) {
     let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
     if (reg.test(email) === false) {
       console.log("Email is Not Correct");
       setEmail(text);
-      setIsEmail(false)
-    }
-    else {
+      setIsEmail(false);
+    } else {
       setEmail(text);
       console.log("Email is Correct");
-      setIsEmail(true)
+      setIsEmail(true);
     }
   }
 
   return (
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.container}>
-          <View style={styles.logo}>
-            <LogoIcon 
-              height={150}
-              width={274.23}
-            />
-          </View>
-          
-          <TextInput style={styles.Input} placeholder="email" placeholderTextColor="#000" keyboardType="email-address" onChangeText={text => validateEmail(text)}></TextInput>  
-          <TextInput style={styles.Input} secureTextEntry={true} placeholder="password" placeholderTextColor="#000" onChangeText={setPassword}></TextInput>
-          {
-            !isEmail ? <Text style={styles.errorText}>Ceci n'est pas un mail valide</Text> : null
-          }
-          <View style={styles.loginBtn}>
-            <LoginBtn eventPush={moveToGardenai}/>
-          </View>
-          <Text onPress={moveToRegister} style={styles.register}>Vous n'avez pas encore de compte ?</Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <View style={styles.logo}>
+          <LogoIcon height={150} width={274.23} />
         </View>
-      </TouchableWithoutFeedback>
+        <TextInput
+          style={styles.Input}
+          placeholder="email"
+          placeholderTextColor="#000"
+          keyboardType="email-address"
+          onChangeText={(text) => validateEmail(text)}
+        />
+        <TextInput
+          style={styles.Input}
+          secureTextEntry={true}
+          placeholder="password"
+          placeholderTextColor="#000"
+          onChangeText={setPassword}
+        />
+        {!isEmail ? (
+          <Text style={styles.errorText}>Ceci n'est pas un mail valide</Text>
+        ) : null}
+        {!isError ? (
+          <Text style={styles.errorText}>Email ou mot de passe invalide</Text>
+        ) : null}
+
+        <View style={styles.loginBtn}>
+          <LoginBtn eventPush={startLogin} />
+        </View>
+        <Text onPress={moveToRegister} style={styles.register}>
+          Vous n'avez pas encore de compte ?
+        </Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     height: "100%",
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',   
-    backgroundColor: "#FFFBF9"
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFFBF9",
   },
   errorText: {
-    color: 'red',
-    marginBottom : "5%",
+    color: "red",
+    marginBottom: "5%",
   },
   Input: {
-    borderWidth:1,
-    borderColor: 'rgba(54, 34, 34, 0.25)',
-    minWidth: '80%',
-    minHeight: '5%',
+    borderWidth: 1,
+    borderColor: "rgba(54, 34, 34, 0.25)",
+    minWidth: "80%",
+    minHeight: "5%",
     borderRadius: 10,
-    backgroundColor: '#FFF9F5',
+    backgroundColor: "#FFF9F5",
     paddingLeft: 20,
-    marginBottom : "5%",
+    marginBottom: "5%",
   },
   logo: {
-    marginStart:"7.5%",
+    marginStart: "7.5%",
     marginBottom: "10%",
-  //   borderWidth:0,
+    //   borderWidth:0,
   },
   register: {
-    textDecorationLine: 'underline',
-    color: 'black',
+    textDecorationLine: "underline",
+    color: "black",
   },
   loginBtn: {
     marginBottom: "5%",
-
-  } 
+  },
 });
 
 export default Login;
